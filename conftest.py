afcca018030d6ff4aade6fd77acb0ce3e11c005b
Ladislao <vladX09rus@gmail.com>
2018-01-05 21:42:08 +0300

    Move additional functions from testing_api fixture

--- conftest.py
+++ conftest.py
@@ -11,55 +11,67 @@ from clickpecker.api import BasicAPI
 from clickpecker.configurations import default_config
 
 
-@pytest.fixture
-def testing_api(output_dir, request):
-    def acquire_device(device_specs, manager_url):
-        acquire_url = "{}/acquire".format(manager_url)
-        request_body = {"filters": device_specs}
-        r = requests.post(acquire_url, json=request_body)
-        if r.status_code == 200:
-            device = Device.from_dict(r.json())
-            return device
-        else:
-            # TODO: add non-free device handling
-            raise ConnectionError(r.text)
+def acquire_device(device_specs, manager_url):
+    acquire_url = "{}/acquire".format(manager_url)
+    request_body = {"filters": device_specs}
+    r = requests.post(acquire_url, json=request_body)
+    if r.status_code == 200:
+        device = Device.from_dict(r.json())
+        return device
+    else:
+        # TODO: add non-free device handling
+        raise ConnectionError(r.text)
 
-    def configure_wrapper(device_model, device_url):
-        wrapper = DeviceWrapper(device_model, device_url)
-        return wrapper
 
-    def release_device(device, manager_url):
-        release_url = "{}/release".format(manager_url)
-        request_body = {"filters": {"adb_id": device.adb_id}}
-        r = requests.post(release_url, json=request_body)
-        if r.status_code != 200:
-            raise ConnectionError(r.text)
+def configure_wrapper(device_model, device_url):
+    wrapper = DeviceWrapper(device_model, device_url)
+    return wrapper
 
-    def create_file_path(base_name, request, output_dir):
-        test_case_name = request.node.name
-        file_name = "{}_{}_{}".format(base_name, test_case_name,
-                                      datetime.now())
-        file_path = output_dir / file_name
-        return file_path
 
-    def save_screenshots_to_pdf(device_wrapper):
-        # Obtain and prepare device's screenshots
-        images = list(device_wrapper.screen_history)
+def release_device(device, manager_url):
+    release_url = "{}/release".format(manager_url)
+    request_body = {"filters": {"adb_id": device.adb_id}}
+    r = requests.post(release_url, json=request_body)
+    if r.status_code != 200:
+        raise ConnectionError(r.text)
 
-        if len(images) == 0:
-            # TODO: replace by logger
-            print("! Screen history is empty !")
-            return
+def create_file_path(base_name, request, output_dir):
+    test_case_name = request.node.name
+    file_name = "{}_{}_{}".format(base_name, test_case_name,
+                                    datetime.now())
+    file_path = output_dir / file_name
+    return file_path
 
-        for i, img in enumerate(images):
-            if img.mode != "RGB":
-                images[i] = img.convert("RGB")
-        img = images[0]
-        path = create_file_path("screenshots", request, output_dir)
+
+def save_screenshots_to_pdf(device_wrapper, request, output_dir):
+    # Obtain and prepare device's screenshots
+    images = list(device_wrapper.screen_history)
+
+    if len(images) == 0:
         # TODO: replace by logger
-        print("Saving {} screenshots into {}".format(len(images), path))
-        img.save(path, "PDF", save_all=True, append_images=images[1:])
+        print("! Screen history is empty !")
+        return
 
+    for i, img in enumerate(images):
+        if img.mode != "RGB":
+            images[i] = img.convert("RGB")
+    img = images[0]
+    path = create_file_path("screenshots", request, output_dir)
+    # TODO: replace by logger
+    print("Saving {} screenshots into {}".format(len(images), path))
+    img.save(path, "PDF", save_all=True, append_images=images[1:])
+
+
+@pytest.fixture
+def output_dir(request):
+    rootdir = request.config.rootdir
+    output_dir = pathlib.Path(rootdir) / "output"
+    output_dir.mkdir(parents=True, exist_ok=True)
+    return output_dir
+
+
+@pytest.fixture
+def testing_api(request, output_dir):
     @contextmanager
     def configure_api(device_specs,
                       manager_url,
@@ -73,14 +85,6 @@ def testing_api(output_dir, request):
             yield api
         finally:
             release_device(device, manager_url)
-            save_screenshots_to_pdf(device_wrapper)
+            save_screenshots_to_pdf(device_wrapper, request, output_dir)
 
     return configure_api
-
-
-@pytest.fixture
-def output_dir(request):
-    rootdir = request.config.rootdir
-    output_dir = pathlib.Path(rootdir) / "output"
-    output_dir.mkdir(parents=True, exist_ok=True)
-    return output_dir
