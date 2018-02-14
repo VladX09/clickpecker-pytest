b518261cf04667e78cbf1d0725324bdbf89015a0
Ladislao <vladX09rus@gmail.com>
2018-02-14 22:34:24 +0300

    Add requests dependency

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="pytest-clickpecker",
     version="0.1",
     packages=["pytest_clickpecker"],
-    install_requires=["clickpecker", "pytest", "packaging"],
+    install_requires=["clickpecker", "pytest", "packaging", "requests"],
     entry_points={
         'pytest11': [
             'pytest-clickpecker = pytest_clickpecker.plugin',
